f22d5ef6006d780840dbcff9cd62d6cbc8373508
Zack Ferro Fields <zackferrofields@gmail.com>
2015-11-13 17:24:35 +0000

    simpler syntax

--- app/Application.jsx
+++ app/Application.jsx
@@ -1,16 +1,12 @@
 import React, {createClass, PropTypes} from 'react';
 
-const Header = ({title}) => (
-  <h1>{title}</h1>
-);
+const Header = ({title}) => <h1>{title}</h1>;
 
 export default createClass({
   propTypes: {
     state: PropTypes.object.isRequired
   },
   render() {
-    return (
-      <Header {...this.props.state}/>
-    );
+    return <Header {...this.props.state}/>;
   }
 });
