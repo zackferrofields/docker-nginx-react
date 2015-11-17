38e55b16ce04ca4b6b39bd915cf6b34d0ecbb5b9
Zack Ferro Fields <zackferrofields@gmail.com>
2015-11-17 18:03:15 +0000

    set cache response via Cache-Control

--- app/server.babel.js
+++ app/server.babel.js
@@ -6,15 +6,17 @@ import {renderToString} from 'react-dom/server';
 import Application from './Application.jsx';
 
 const PORT = process.env.PORT || 3000;
+const DEFAULT_CACHE_POLICY = 'public,max-age=60';
 const INDEX_PATH = './views/index.html';
 const TITLE = 'Hello World Page';
 
 function index(response) {
   const state = { title: 'Hello World!' };
+  const header = {'Content-Type': 'text/html', 'Cache-Control': DEFAULT_CACHE_POLICY};
   const content = renderToString(createElement(Application, {state}));
   readFile(INDEX_PATH, 'utf8', (err, data) => {
     if (err) throw err;
-    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.writeHead(200, header);
     response.write(format(data, TITLE, content));
     response.end();
   });
